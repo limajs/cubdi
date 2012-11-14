3a2c1e6a59327fc7ae30a8d2fc32f4f7ee4eb987
Simon Crabtree <simon@simoncrabtree.net>
2012-11-14 19:07:38 +0000

    Revert "Renamed controller and augmented test"
    This reverts commit 19b38a44d81281689da12800f5da20073846a193.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-cubdiApp.controller('MainController', function($scope) {
+cubdiApp.controller('MainCtrl', function($scope) {
   $scope.awesomeThings = [
     'HTML5 Boilerplate',
     'AngularJS',
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,28 +1,22 @@
 'use strict';
 
-describe('Main Controller', function() {
+describe('Controller: MainCtrl', function() {
 
-    // load the controller's module
-    beforeEach(module('cubdiApp'));
+  // load the controller's module
+  beforeEach(module('cubdiApp'));
 
-    var MainController,
+  var MainCtrl,
     scope;
 
-    // Initialize the controller and a mock scope
-    beforeEach(inject(function($controller) {
-        scope = {
-            todaysMeals: [
-                'Lasagne',
-                'Sticky Toffee Pudding'
-            ]
-        };
-        MainController = $controller('MainController', {
-            $scope: scope
-        });
-    }));
-
-    it('should attach a list of awesomeThings to the scope', function() {
-        expect(scope.awesomeThings.length).toBe(3);
-        expect(scope.todaysMeals.length).toBe(2);
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function($controller) {
+    scope = {};
+    MainCtrl = $controller('MainCtrl', {
+      $scope: scope
     });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function() {
+    expect(scope.awesomeThings.length).toBe(3);
+  });
 });
