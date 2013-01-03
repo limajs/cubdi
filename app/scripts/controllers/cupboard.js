c0ba12c36d6fccafa4230075c57bc069cfb7012e
Simon Crabtree <limajs@simoncrabtree.net>
2013-01-03 15:47:46 +0000

    Css for shopping-list-items now made more generic for all items in lists of food

--- app/scripts/controllers/cupboard.js
+++ app/scripts/controllers/cupboard.js
@@ -6,17 +6,19 @@ cubdiApp.controller('CupboardCtrl', function($scope, $http) {
         $scope.itemsInCupboard = data.items;
 
         $scope.itemsAdded = [];
-        $scope.addItem = function () {
-            var newItem = {
-                description: $scope.item.description,
-                status: "adding"
-            };
-            $http.post('/command/addItemToShoppingList', $scope.item).success(function () {
-                console.log("Item Added", $scope.item);
-                newItem.status = "added";
-            });
-            $scope.itemsAdded.push(newItem);
-            $scope.item = {};
-        };
+
     });
+
+    $scope.addItem = function () {
+        var newItem = {
+            description: $scope.item.description,
+            status: "adding"
+        };
+        $http.post('/command/addItemToShoppingList', $scope.item).success(function () {
+            console.log("Item Added", $scope.item);
+            newItem.status = "added";
+        });
+        $scope.itemsAdded.push(newItem);
+        $scope.item = {};
+    };
 });
